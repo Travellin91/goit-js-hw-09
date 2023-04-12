1963b2699160b56518f47b9a44574d740e83ca6f
Travellin91 <118572078+Travellin91@users.noreply.github.com>
2023-04-12 19:04:45 +0300

    task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,4 +2,3 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
-
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,8 @@
-import Notiflix from 'notiflix';
-import 'notiflix/dist/notiflix-3.2.5.min.css';
-
-import Notiflix from 'notiflix';
-import 'notiflix/dist/notiflix-3.2.5.min.css';
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    // Fulfill
+  } else {
+    // Reject
+  }
+}
