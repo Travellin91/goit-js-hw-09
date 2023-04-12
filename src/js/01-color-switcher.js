e111fa7da22a3950ebaf577b5e4fdbbafa16cd3c
Travellin91 <118572078+Travellin91@users.noreply.github.com>
2023-04-12 18:46:33 +0300

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+const colorValSpan = document.querySelector('.color');
+
+let timerId = null;
+
+const backgroundColorSwitcher = function () {
+  body.style.backgroundColor = getRandomHexColor();
+  body.style.color = getRandomHexColor();
+  colorValSpan.textContent = getRandomHexColor();
+};
+
+btnStart.addEventListener('click', () => {
+  timerId = setInterval(backgroundColorSwitcher, 1000);
+  btnStart.disabled = true;
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+});
